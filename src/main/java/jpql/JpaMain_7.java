7954cb15c18899970c736d932ca83bc32d05c85d
psjw <dlrudtn108@gmail.com>
2025-03-30 18:49:15 +0900

    join

--- src/main/java/jpql/JpaMain_7.java
+++ src/main/java/jpql/JpaMain_7.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Persistence;
 
 import java.util.List;
 
-public class JpaMain_6 {
+public class JpaMain_7 {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -16,21 +16,25 @@ public class JpaMain_6 {
         tx.begin();
         //code
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member"+i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.changeTeam(team);
+
+            em.persist(member);
 
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+//            String query = "select m from Member m join Team t ";
+//            String query = "select m from Member m left outer join Team t ";
+            String query = "select m from Member m , Team t where m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
             System.out.println("result.size() = " + result.size());
             for (Member member1 : result) {
